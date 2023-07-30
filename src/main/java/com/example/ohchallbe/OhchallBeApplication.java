405d2993b8309be9ef76d7ff98dc984fbefc84a9
DESKTOP-LOH5O20\rehit <rehiter48@gmail.com>
2023-07-30 14:40:10 +0900

    main init

---
+++ src/main/java/com/example/ohchallbe/OhchallBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohchallbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhchallBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhchallBeApplication.class, args);
+	}
+
+}
