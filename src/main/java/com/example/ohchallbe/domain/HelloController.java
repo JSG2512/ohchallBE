7ccd73c6cb209b0a954e42dc039e97cd4f32cd2b
DESKTOP-LOH5O20\rehit <rehiter48@gmail.com>
2023-07-31 13:41:09 +0900

    main init ver5

--- src/main/java/com/example/ohchallbe/domain/HelloController.java
+++ src/main/java/com/example/ohchallbe/domain/HelloController.java
@@ -2,11 +2,10 @@ package com.example.ohchallbe.domain;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+
 public class HelloController {
 
     @GetMapping("/hello")
